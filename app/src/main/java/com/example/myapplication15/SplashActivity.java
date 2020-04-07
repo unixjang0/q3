caa7fe3ac3aa35e38eae60125f802dccd635d89e
unixjang <unixjang@naver.com>
2020-04-07 16:41:04 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication15/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication15;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        }, 2000);
+    }
+}
