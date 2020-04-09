24008b27a6bd0d0b31333aeb3ee47813756ae67a
2wldud98 <2wldud98@gmail.com>
2020-04-09 22:06:46 +0900

    4 menu

--- app/src/main/java/com/example/myapplication15/MainActivity.java
+++ app/src/main/java/com/example/myapplication15/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication15;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -30,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
             case R.id.menu_user:
                 Toast.makeText(this, "user 메뉴 선택됨.", Toast.LENGTH_LONG).show();
                 break;
+            case R.id.menu_menu:
+                Intent intent = new Intent(getApplicationContext(),);
             default:
                 break;
         }
@@ -42,14 +45,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //ActionBar abar = getSupportActionBar();
-        //abar.hide();
+        ActionBar abar = getSupportActionBar();
+        abar.setDisplayShowTitleEnabled(false);
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_menu, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
