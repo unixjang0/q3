f7c3a92378dc29c0ae906bc4f46a1f47cf0332ff
2wldud98 <2wldud98@gmail.com>
2020-04-10 11:43:13 +0900

    4 menu

--- app/src/main/java/com/example/myapplication15/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/myapplication15/ui/search/SearchFragment.java
@@ -22,8 +22,8 @@ public class SearchFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(SearchViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
+        View root = inflater.inflate(R.layout.fragment_search, container, false);
+        final TextView textView = root.findViewById(R.id.text_search);
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
